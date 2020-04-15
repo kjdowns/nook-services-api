ddcdb52ec33c0e3a9f962acd5cde615b17412c05
kjdowns <kevin.downs04@gmail.com>
2020-04-14 20:36:27 -0400

    add initial json render for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: {username: user.username, bugs: user.bugs, fish: user.fish}
+    end
+
 end
