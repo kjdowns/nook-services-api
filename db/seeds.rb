d3054c46ebf8395920bce44df9df46dcfceacd77
kjdowns <kevin.downs04@gmail.com>
2020-04-19 17:52:50 -0400

    add initial island data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,127 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+island_data = [
+    {
+        name: "Spiral River Island ",
+        rate: "9%",
+        description: 'An island with a river that winds in a spiral. It is part of the subset of four "starter" islands that players will randomly arrive at for their very first mystery tour. ',
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/2/22/Spiral_Island.png/revision/latest/scale-to-width-down/120?cb=20200417211554"
+    },
+    {
+        name: "Fidget Spinner Island ",
+        rate: "9%",
+        description: 'An island with a large pond in the middle and a small 2nd level to the northwest. The lake\'s shape has been compared to a fidget spinner. It is part of the subset of four "starter" islands that players will randomly arrive at for their very first mystery tour. It can also provide lots and lots of exotic freshwater fish(e.g. koi , ranchu, and crawfish.), be sure to bring a nice fishing rod with you and maybe even some clams. ',
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/9/95/Mapthumb4.png/revision/latest?cb=20200419050812"
+    },
+    {
+        name: "Short River Island #1",
+        rate: "9%",
+        description: "An island two layers, with a river that begins on a small second layer in the north, flows down a waterfall and turns to the east. It is part of the subset of four \"starter\" islands that players will randomly arrive at for their very first mystery tour. ",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/e/e5/Mapthumb1.png/revision/latest/scale-to-width-down/119?cb=20200419050651"
+    },
+    {
+        name: "Short River Island #2",
+        rate: "9%",
+        description: "Has almost the exact same layout as short river island #1, but with the addition of a pond in the southwest. It is part of the subset of four \"starter\" islands that players will randomly arrive at for their very first mystery tour. ",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/3/32/Normal_Island_1.png/revision/latest/scale-to-width-down/120?cb=20200417212844"
+    },
+    {
+        name: "Tarantula/Scorpion Island",
+        rate: "2% (rare)",
+        description: "A one-layer island where the only bugs that spawn are tarantulas or scorpions depending on the time of year. It has a circular stream that surrounds a smaller island in the center. The stream is narrow enough that it can be jumped across without a vaulting pole. This can only appear from 7 pm to 4 am, the time when both insects are active.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/9/95/Mapthumb13.png/revision/latest/scale-to-width-down/123?cb=20200419051023"
+    },
+    {
+        name: "Bamboo Island",
+        rate: "10%",
+        description: "A one-layer island where bamboo is found. While there are palm trees on the beaches, there are no fruit, hardwood, or cedar trees. This is the most common island you can get out of the 20, statistically. ",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/5/5e/Mapthumb8.png/revision/latest/scale-to-width-down/120?cb=20200415194939"
+    },
+    {
+        name: "Money Rock Island #1",
+        rate: "5%",
+        description: "Requires the Resident Services building upgrade. An island of nothing but money rocks surrounded by water. By eating fruit, the player can break a rock to the north which will open up space for the player to vault to the central island. The rocks in the central island are all money rocks. The maximum money obtained here is 80,500 Bells. If you break the rock at the north to get to the center island before hitting it once, you get an additional 8,000 Bells, totaling 88,500 for the whole island.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/b/b4/Mapthumb7.png/revision/latest/scale-to-width-down/120?cb=20200415195029"
+    },
+    {
+        name: "Money Rock Island #2",
+        rate: "2% (rare)",
+        description: "Requires the Resident Services building upgrade. This island has four small triangular cliffs in the northeast, southeast, southwest, and northwest corners, and no water features. The ground between the cliffs is scattered with money rocks and flowers. This island also has an abundace of tarantualas or scorpions, depending on the month and hemisphere in game.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/2/21/Mapthumb12.png/revision/latest/scale-to-width-down/111?cb=20200419051106"
+    },
+    {
+        name: "Trash Island",
+        rate: "5%",
+        description: "An island where the only \"fish\" that spawn in the river are trash items. This island is good for farming Nook Miles and DIY recipes that use trash. Primarily a lot of water-related insects spawn.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/0/00/Mapthumb20.png/revision/latest/scale-to-width-down/119?cb=20200419051213"
+    },
+    {
+        name: "Finned Fish Island",
+        rate: "1% (rare)",
+        description: "Requires the Resident Services building upgrade. An island with a rectangular pond with rectangular cliffs inside, the tallest one being too tall to climb up to. The only fish that spawn here are the fish that have fins sticking out of the water such as sharks, ocean sunfish, and suckerfish. This island can only appear during times of the year where finned fish spawn. No fish will spawn in the river, not even if bait is used.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/1/1b/Mapthumb21.png/revision/latest/scale-to-width-down/121?cb=20200419051252"
+    },
+    {
+        name: "Hybrid Flower Island ",
+        rate: "2% (rare)",
+        description: "A small island consisting of a pond surrounded by hybrid flowers of a single type. The only insects that spawn here are insects that appear near flowers such as butterflies, moths, and honeybees.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/c/ca/Mapthumb11.png/revision/latest/scale-to-width-down/121?cb=20200419051914"
+    },
+    {
+        name: "Waterfall Island",
+        rate: "5%",
+        description: "A three-layer island dominated by cliffs, rivers, and waterfalls. The river flows in a counter-clockwise path, from the third layer down into a small open area of water, which splits into three other sections, with the right-most section leading into a river-mouth. The other two sections lead to a small basin. This island is useful for getting cliff-top fish like the stringfish, and since it has also a single mouth for the river, it also makes river-mouth spawning fish like sturgeon easier to farm.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/0/01/Falls_Island.png/revision/latest/scale-to-width-down/120?cb=20200417213019"
+    },
+    {
+        name: "Mountain Island",
+        rate: "9%",
+        description: "A three-tiered island covered in fruit trees. The top layer has many flowers and five rocks. There are no ponds or rivers, so only ocean fish can be caught. The only bugs that spawn are those that appear when hitting rocks such as centipedes and pill bugs.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/2/2b/Mountian_Island.png/revision/latest/scale-to-width-down/120?cb=20200417213039"
+    },
+    {
+        name: "Sister Fruit Island",
+        rate: "9%",
+        description: "An island with 19 fruit trees that are of a different type than your native fruit. The fruit here will always be the same ones referred to as \"sister fruit\". The island has a short river with an outlet on the west coast.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/0/0f/Mapthumb10.png/revision/latest/scale-to-width-down/136?cb=20200419051424"
+    },
+    {
+        name: "Big Fish Island #1",
+        rate: "3% (rare)",
+        description: "Requires the Resident Services building upgrade. A 3-layer island with a selection of fruit-bearing trees, cedar trees, and palm trees. The highest-most level is a small waterfall with one outlet to the second level. There is a small outlet to the left and then finally flows to a small outlet at the bottom left. There are only size 4+ fish here making getting rare size 6 fish easier. The island also has rare hybrid flowers and only spawns bugs that appear on flowers such as mantis and ladybugs.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/5/58/Big_Fish_Island%28Bass%29.png/revision/latest/scale-to-width-down/120?cb=20200417213101"
+    },
+    {
+        name: "Big Fish Island #2",
+        rate: "2% (rare)",
+        description: "An island where the only fish that spawn are size 4 or larger, making it a great place to catch rare size 6 fish. The river originates on a small plateau in the northeastern corner of the island, flows down waterfalls on the southern and western edges of the plateau, and rejoins to flow through a single mouth at the western edge, forming a small river island in the center. The island has four rocks, four palm trees, four trees of the player's native fruit and six other deciduous trees.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/a/ac/Big_Fish_Island_2.png/revision/latest/scale-to-width-down/120?cb=20200417213126"
+    },
+    {
+        name: "Gold Rock Island",
+        rate: "1% (rare)",
+        description: "Requires the Resident Services building upgrade. A rare island with a single rock in the middle which only drops gold nuggets when hit. The rock is surrounded by a rectangular pond with a cliff to the south. The ladder and vaulting pole is required to access the rock. The only insects that spawn are scorpions or tarantulas, depending on the season.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/7/72/Mapthumb24.png/revision/latest/scale-to-width-down/120?cb=20200419051515"
+    },
+    {
+        name: "Hardwood Tree Island #1",
+        rate: "2% (rare)",
+        description: "Requires the Resident Services building upgrade. An island with no water features and no fruit trees other than coconut trees. It has three small cliff areas, two with a single level and one with two levels. The hardwood trees are plentiful and densely-packed. The only insects that spawn are those associated with trees such as the Goliath beetle, Rainbow stag, and Atlas moth.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/a/a3/Mapthumb14.png/revision/latest/scale-to-width-down/96?cb=20200419051806"
+    },
+    {
+        name: "Hardwood Tree Island #2",
+        rate: "1% (rare)",
+        description: "This island has the exact same layout as Bamboo Island, but there are only hardwood trees on it instead of bamboo. The only insects that spawn are those associated with trees such as the Walking stick, Earth-boring dung beetle, and Horned atlas.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/5/5e/Mapthumb8.png/revision/latest/scale-to-width-down/111?cb=20200415194939"
+    },
+    {
+        name: "Curly River Island",
+        rate: "5%",
+        description: "An island with a single, square-shaped cliff in the northeast with a waterfall that feeds a river that flows in a counter-clockwise loop around the island to an outlet on the east coast. Only dragonflies and insects associated with water spawn here.",
+        url: "https://vignette.wikia.nocookie.net/animalcrossing/images/b/bf/Mapthumb18.png/revision/latest/scale-to-width-down/133?cb=20200419051842"
+    }
+]
+
+island_data.each do |island|
+    Island.create(name: island.name, rate: island.rate, description: island.description, url: island.url)
+end
