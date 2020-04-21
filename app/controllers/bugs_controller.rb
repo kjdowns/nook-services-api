92a271fd4932ffcd6df0a41ffba2391fc68db667
kjdowns <kevin.downs04@gmail.com>
2020-04-21 18:13:54 -0400

    add user object as response to update route

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,8 +1,10 @@
 class BugsController < ApplicationController
 
     def update
-        binding.pry
         bug = Bug.find(params[:id])
+        user = User.find(bug.user_id)
+        Bug.update(params[:id], checked: params[:checked])
+        render json: {username: user.username, bugs: user.bugs, fish: user.fish}
     end
 
 end
