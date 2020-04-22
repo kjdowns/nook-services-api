94f1a9c18908108d38fa9058d45649600c925859
kjdowns <kevin.downs04@gmail.com>
2020-04-21 23:26:12 -0400

    add update action to fish

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,2 +1,10 @@
 class FishController < ApplicationController
+
+    def update
+        fish = Fish.find(params[:id])
+        user = User.find(fish.user_id)
+        Fish.update(params[:id], checked: params[:checked])
+        render json: {username: user.username, bugs: user.bugs, fish: user.fish}
+    end
+
 end
