d862e47c5129bc080d3749264d7ce05607c0de71
kjdowns <kevin.downs04@gmail.com>
2020-04-21 23:28:28 -0400

    restrict resources to used routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :fish
-  resources :bugs
-  resources :users
-  resources :islands
+  resources :fish, only: [:update]
+  resources :bugs, only: [:update]
+  resources :users, only: [:show, :create]
+  resources :islands, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
